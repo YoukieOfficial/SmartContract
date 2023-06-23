6d65a6d662cdadacb39fa717705b3c3a076caa66
Youkie <137475691+YoukieOfficial@users.noreply.github.com>
2023-06-23 13:03:48 +0700

    Update YoukieWallet.sol

--- YoukieWallet.sol
+++ YoukieWallet.sol
@@ -1,1 +1,22 @@
+// Deployed by Youkie
+// https://twitter.com/YoukieOfficial
 
+pragma solidity 0.8.12;
+
+contract YoukieWallet {
+    address public owner;
+    uint public balance;
+    constructor() {
+        owner = msg.sender;
+    }
+
+function depositEther() external payable{
+        balance += msg.value;
+    }
+
+function withdrawEther(uint amount) external{
+        require(msg.sender == owner, "Only owner can call this function");
+        require(amount <= balance, "Amount exceeds balance");
+        balance -= amount;
+        payable(owner).transfer(amount);
+    }
